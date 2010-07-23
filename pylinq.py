b43c6bf560b903c37a4c709f57581e2a6e7b97f7
amoreira <andres@insophia.com>
2010-07-23 13:18:13 -0300

    added select and order by method

--- pylinq.py
+++ pylinq.py
@@ -10,11 +10,31 @@ class PyLINQException(Exception):
 
 class PyLINQ(object):
     def __init__(self, items):
-        assert (items is not None and isinstance(items[0], dict))
-        self.items
+        self.items = items
+
+    def Items(self):
+        return self.items
+
+    def toList(self):
+        return list(self.items)
 
     def Where(self, clause):
         it = iter(self.items)
         if not callable(clause):
             raise PyLINQException("caluse argument must be callable.")
-        return (e for e in it if clause(e))
+        return PyLINQ((e for e in it if clause(e)))
+
+    def Select(self, clause):
+        it = iter(self.items)
+        if not callable(clause):
+            raise PyLINQException("caluse argument must be callable.")
+        return PyLINQ((e for e in it if clause(e)))
+
+    def OrderBy(self, clause, cmp=None, order='asc'):
+        it = iter(self.items)
+        if not callable(clause):
+            raise PyLINQException("caluse argument must be callable.")
+        ls = sorted(it, key=clause, cmp=cmp, reverse=(order != 'asc'))
+        return PyLINQ(ls)
+
+
