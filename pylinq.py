caa75dcfb7b95b7ac86e76efe4f10612778b4a82
amoreira <andres@insophia.com>
2010-07-23 15:15:29 -0300

    just completed all the operations to be compatible with JSLINQ

--- pylinq.py
+++ pylinq.py
@@ -42,11 +42,76 @@ class PyLINQ(object):
         return PyLINQ(ls)
 
     def Count(self, clause=None):
-        if not self._exitems:
-            self._exitems = list(self.items)
-            self.items = iter(self._exitems)
         if not clause:
-            return len(self._exitems)
+            return len(self.toList())
         else:
-            return len([e for e in self._exitems if clause(e)])
+            return len([e for e in self.toList() if clause(e)])
+
+    def Distinct(self, clause):
+        it = iter(self.items)
+        if not callable(clause):
+            raise PyLINQException("caluse argument must be callable.")
+        try:
+            diselems = set(clause(x) for x in it)
+            return PyLINQ(diselems)
+        except TypeError, e:
+            raise PyLINQException("clause should return a hashable item")
+
+    def Any(self, clause):
+        if not callable(clause):
+            raise PyLINQException("caluse argument must be callable.")
+        for x in self.toList():
+            if clause(x):
+                return x
+        return
+
+    def All(self, clause):
+        for x in self.toList():
+            if not clause(x):
+                return False
+        return True
+
+    def Reverse(self):
+        return PyLINQ(self.toList()[::-1])
+
+    def _get_elem(self, clause, pos):
+        if clause:
+            return self.Where(clause).First()
+        elist = self.toList()
+        return elist[pos] if elist else None
+
+    def First(self, clause=None):
+        return self._get_elem(0)
+
+    def Last(self, clause=None):
+        return self._get_elem(-1)
+
+    def ElementAt(self, index):
+        elist = self.toList()
+        return elist[index]
+    
+    def Concat(self, items):
+        elist = self.toList()
+        return PyLINQ(elist + items)
+
+    def DefaultIfEmpty(self, default=None):
+        default = default or []
+        if not self.toList():
+            return default
+        return self
+
+    def ElementAtOrDefault(self, index, defaultitem=None):
+        try:
+            elist = self.toList()
+            item = elist[index]
+            return item
+        except IndexError:
+            return defaultitem
+
+    def FirstOrDefault(self, defaultitem=None)
+        return self.First() or defaultitem
+
+    def LastOrDefault(sefl, defaultitem=None)
+        return self.Last() or defaultitem
+
 
