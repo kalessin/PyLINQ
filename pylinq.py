0ff3b43ccd701855d7a0eca8bcfe5d7f67f83dfa
amoreira <andres@insophia.com>
2010-07-23 15:59:09 -0300

    fix methods after method refactoring

--- pylinq.py
+++ pylinq.py
@@ -32,7 +32,7 @@ class PyLINQ(object):
     def Select(self, clause):
         _check(clause)
         return PyLINQ((clause(e) for e in self.iteritems()))
-    
+
     def OrderBy(self, clause, cmp=None, order='asc'):
         _check(clause)
         ls = sorted(self.iteritems(), key=clause, cmp=cmp, reverse=(order != 'asc'))
@@ -45,36 +45,34 @@ class PyLINQ(object):
             return len([e for e in self.items() if clause(e)])
 
     def Distinct(self, clause):
-        it = iter(self.items)
-        if not callable(clause):
-            raise PyLINQException("caluse argument must be callable.")
+        _check(clause)
         try:
-            diselems = set(clause(x) for x in it)
+            diselems = set(clause(x) for x in self.iteritems())
             return PyLINQ(diselems)
         except TypeError, e:
             raise PyLINQException("clause should return a hashable item")
 
     def Any(self, clause):
-        if not callable(clause):
-            raise PyLINQException("caluse argument must be callable.")
-        for x in self.toList():
+        _check(clause)
+        for x in self.iteritems():
             if clause(x):
                 return x
         return
 
     def All(self, clause):
-        for x in self.toList():
+        _check(clause)
+        for x in self.iteritems():
             if not clause(x):
                 return False
         return True
 
     def Reverse(self):
-        return PyLINQ(self.toList()[::-1])
+        return PyLINQ(self.items()[::-1])
 
     def _get_elem(self, clause, pos):
         if clause:
             return self.Where(clause).First()
-        elist = self.toList()
+        elist = self.items()
         return elist[pos] if elist else None
 
     def First(self, clause=None):
@@ -84,22 +82,21 @@ class PyLINQ(object):
         return self._get_elem(-1)
 
     def ElementAt(self, index):
-        elist = self.toList()
+        elist = self.items()
         return elist[index]
     
     def Concat(self, items):
-        elist = self.toList()
-        return PyLINQ(elist + items)
+        return PyLINQ(self.items() + list(items))
 
     def DefaultIfEmpty(self, default=None):
         default = default or []
-        if not self.toList():
+        if not self.items():
             return default
         return self
 
     def ElementAtOrDefault(self, index, defaultitem=None):
         try:
-            elist = self.toList()
+            elist = self.items()
             item = elist[index]
             return item
         except IndexError:
@@ -108,7 +105,7 @@ class PyLINQ(object):
     def FirstOrDefault(self, defaultitem=None):
         return self.First() or defaultitem
 
-    def LastOrDefault(sefl, defaultitem=None):
+    def LastOrDefault(self, defaultitem=None):
         return self.Last() or defaultitem
 
 
