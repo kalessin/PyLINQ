b53b4b499b93ad12b422386a122316379a1161fc
molveyra <molveyra@gmail.com>
2010-07-23 15:11:51 -0300

    added tests

---
+++ test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from pylinq import PyLINQ
+
+data = [
+{"name": "item1", "class": "classA", "size": 8},
+{"name": "item2", "class": "classA", "size": 10},
+{"name": "item3", "class": "classB", "size": 6},
+]
+
+class PyLINQTest(TestCase):
+    def test1(self):
+        pq = PyLINQ(data)
+        self.assertEqual(
+            pq.Where(lambda it: it["class"] == "classA")
+            .Select(lambda it: {"name": it["name"], "size": it["size"]}).items(),
+            [{"name": "item1", "size": 8},
+             {"name": "item2", "size": 10}])
